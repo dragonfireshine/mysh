fafd99d4326839bba71c5a578b825769ff3ea6fd
dragonfireshine <dragonfireshine@qq.com>
2020-04-17 17:37:37 +0800

    为zabbix的准备

---
+++ zabbix_tcp_connect_status.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+LINTEN(){
+	ss -an |grep '^tcp' |grep 'LISTEN' |wc -l
+}
+
+SYN_RECV(){
+	ss -an |grep '^tcp' |grep 'SYN[_-]RECV' |wc -l
+}
+
+ESTABLISHED(){
+	ss -an |grep '^tcp' |grep 'ESTAB' |wc -l
+}
+
+TIME_WAIT(){
+	ss -an |grep '^tcp' |grep 'TIME[_-]WAIT' |wc -l
+}
+
+$1
