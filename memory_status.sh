48cbced36ed375f4fd8f136f2874ce986eacdbf9
dragonfireshine <dragonfireshine@qq.com>
2020-04-18 00:05:11 +0800

    内存收集，供zabbix

---
+++ memory_status.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#memory for zabbix
+MemTotal(){
+	awk '/^MemTotal/{print $2}' /proc/meminfo
+}
+
+MemFree(){
+	awk '/^MemFree/{print $2}' /proc/meminfo
+}
+
+Buffers(){
+	awk '/^Buffers/{print $2}' /proc/meminfo
+}
+
+Dirty(){
+	awk '/^Dirty/{print $2}' /proc/meminfo
+}
+
+Cached(){
+	awk '/^Cached/{print $2}' /proc/meminfo
+}
+
+SwapCached(){
+	awk '/^SwapCached/{print $2}' /proc/meminfo
+}
+
+Active(){
+	awk '/^Active/{print $2}' /proc/meminfo
+}
+
+Inactive(){
+	awk '/^Inactive/{print $2}' /proc/meminfo
+}
+
+SwapTotal(){
+	awk '/^SwapTotal/{print $2}' /proc/meminfo
+}
+
+SwapFree(){
+	awk '/^SwapFree/{print $2}' /proc/meminfo
+}
+
+$1
